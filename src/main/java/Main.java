f34cbfadb4c3d8634101d22f4aa296e4d09be5a3
Jonatan Rodríguez <jonodomongos@hotmail.com>
2025-10-20 11:57:20 +0200

    commit inicial

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+    /*
+    INSTRUCCIONES:
+    1. (fácil) Entrenamiento
+    Tu amiga cacatúa está de vuelta!
+    Tienes la clase Cacatua con un método static que cuando le llamas dice "Cacatúa cacahuete!".
+    También tiene una variable estatica: cantidadMinimaCacahuetes = 100;
+    Mostrar en un system.out lo siguiente: " la cacatúa se llama X (su nombre), dice 'Cacatúa cacahuete!' y lo mínimo para que se calle son 100 cacahuetes "
+
+    2. (medio) Llega Black Friday en tu tienda online de teléfonos móviles y decides ofrecer un 20% de descuento en todos tus productos.
+    Instancia algunos móviles para probar.
+    El descuento se aplica en todos, (static) así que debes poder mostrar el precio antiguo y el precio con descuento.
+
+    +EXTRA: posibilidad de anular el descuento a todos los productos después del Black Friday, mediante método a tal propósito.
+    */
+    }
+}
